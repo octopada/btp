830dcef4ce10618b21ce498be47c114be013cc03
octopada <gogollian@gmail.com>
2017-10-30 12:08:48 +0530

    sequence numbers added

--- pcap_parser.py
+++ pcap_parser.py
@@ -9,7 +9,7 @@ def remove_port_number(address):
         modified_address = modified_address + '.' + l[n+1];
     return modified_address
 
-def generate_trace_dict_of_lists_of_dicts():
+def generate_trace_dict_of_list_of_dicts():
     files = os.listdir(os.getcwd())
 
     #p2p_pcaps = []
@@ -43,7 +43,7 @@ def generate_trace_dict_of_lists_of_dicts():
         node_addresses[l[0]+'.pcap'] = l[1]
 
     for pcap in pcaps:
-        command = 'tcpdump -nn -tt -r ' + pcap + '| grep \'IP\''
+        command = 'tcpdump -nn -tt -r ' + pcap + '| grep \'IP\' | grep \'seq\''
         output = subprocess.getoutput(command)
         tracelist = output.split('\n')
         
@@ -58,23 +58,26 @@ def generate_trace_dict_of_lists_of_dicts():
             trace_dict = {}
             colon_split = trace.split(':')
             space_split = colon_split[0].split()
+            comma_split = trace.split(',')
             
     #        print('sending address: ' + space_split[2])
             source = remove_port_number(space_split[2])
             destination = remove_port_number(space_split[4])
+            seq_no = comma_split[1].split()[1]
+            
+            if seq_no == '0':
+                continue
             
             trace_dict['timestamp'] = space_split[0];
             trace_dict['source'] = source
             trace_dict['destination'] = destination
-            
+            trace_dict['sequence-no'] = seq_no
+
             trace_list_of_dicts.append(trace_dict)
             
         tracedata[node_addresses[pcap]] = trace_list_of_dicts
         
     return tracedata
-    
-    
-    
     
     
     
